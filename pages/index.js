c6ca3445c44bc66be2bd6e29aab98d807c0d7ae7
Abraham Fajar Santoso <abrahamfsantoso@gmail.com>
2021-08-14 13:32:32 +0700

    added head data

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,20 @@
 import { Fragment } from 'react';
-import Hero from '../components/home-page/hero';
+import Head from 'next/head';
+
 import FeaturedPosts from '../components/home-page/featured-posts';
+import Hero from '../components/home-page/hero';
 import { getFeaturedPosts } from '../lib/posts-util';
 
 function HomePage(props) {
   return (
     <Fragment>
+      <Head>
+        <title>Abie's Blog</title>
+        <meta
+          name='description'
+          content='I post about programming and web development.'
+        />
+      </Head>
       <Hero />
       <FeaturedPosts posts={props.posts} />
     </Fragment>
